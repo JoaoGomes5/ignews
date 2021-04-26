271560b79d024aa43869e6e0a77da06be5bb5bf2
Joao Gomes <joaopfg.2002@gmail.com>
2021-04-27 00:11:54 +0100

    SCSS, TS, Fonts structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+import styles from "../../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+       <title>Inicio | ig.news</title>
+      </Head>
+
+      <h1 >
+        Hello World
+      </h1>
+  
+    </>
+  )
+}
