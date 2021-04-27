9ea7d1ed15bf2f0e5305f203617a3e85943701a1
Joao Gomes <joaopfg.2002@gmail.com>
2021-04-27 14:48:29 +0100

    test commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
-export default function Header() {
+import SignInButton from '../SignInButton';
+import styles from './styles.module.scss';
 
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        
+        <SignInButton />
+        
+      </div>
+
+    </header>
+  )
 }
