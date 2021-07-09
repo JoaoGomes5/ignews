7178fc2cbf94fed34030adcb809ec8064f3c4dd0
Joao Gomes <joaopfg.2002@gmail.com>
2021-07-09 03:32:50 +0100

    Fix

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession , signIn} from 'next-auth/client';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,12 +7,36 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscription(){
+
+    if(!session){
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      
+    } catch (error) {
+      
+    }
+    
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscription}
     >
       Subscribe now
     </button>
   )
 }
+
+
